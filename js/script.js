ec4b32adce869b70c4aa0422d82a852d059ba007
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-04 15:51:22 +0100

    created function get random email

--- js/script.js
+++ js/script.js
@@ -5,7 +5,20 @@ const email = new Vue({
     el: '#root',
 
     data: {
-        email: [],
+
+        totalEmail: 10,
+
+        emails: [],
+    },
+
+    methods: {
+
+        getRandomEmail () {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(res => {
+                this.emails.push(res.data.response)
+            }
+            )},
+
     },
 
 })
